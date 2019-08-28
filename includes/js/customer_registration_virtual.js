85a364139d48efda98b5e10bfa83132173dce376
agarcia <info@btosoftware.net>
2019-08-28 10:37:31 -0600

    New release v1.0.1.7

--- includes/js/customer_registration_virtual.js
+++ includes/js/customer_registration_virtual.js
@@ -1,5 +1,6 @@
-jQuery(document).ready(function(){
 
+jQuery(document).ready(function(){
+	
 	jQuery( "#date_of_birth" ).datepicker({
       changeMonth: true,
       changeYear: true,
@@ -10,7 +11,7 @@ jQuery(document).ready(function(){
 		sp_crv_country_change();
 	});
 	var st = jQuery("#address_state").val();
-		
+	
 	if(st===null) sp_crv_country_change();
 
 	jQuery("#address_state").change(function(){
@@ -80,13 +81,13 @@ function sup_prev(element){
 
 function sp_crv_country_change(){
 	var ctry = jQuery("#address_country").val();
+	var destination=window.location.href;
 	jQuery("#address_state").html("");
 	jQuery("#address_state").attr("disabled", "disabled");
 	jQuery("#address_city").html("");
-	jQuery("#address_city").attr("disabled", "disabled");
-	
+	jQuery("#address_city").attr("disabled", "disabled");	
 	if(!isNaN(ctry)){
-	  jQuery.post("/",{endpoint: "sp_geographic_get_states",country_code:ctry}, function(data, status){
+	  jQuery.post(destination,{endpoint: "sp_geographic_get_states",country_code:ctry}, function(data, status){
 	    	if(data){	    
 	    		
 	    		for(var state in data){
@@ -104,10 +105,11 @@ function sp_crv_country_change(){
 
 function sp_crv_state_change(){
 	var state = jQuery("#address_state").val();	
+	var destination=window.location.href;
 	jQuery("#address_city").html("");
 	jQuery("#address_city").attr("disabled", "disabled");
 	if(!isNaN(state)){
-	  jQuery.post("/",{endpoint: "sp_geographic_get_cities",state_code:state}, function(data, status){
+	  jQuery.post(destination,{endpoint: "sp_geographic_get_cities",state_code:state}, function(data, status){
 	    	if(data){	    		
 	    		for(var state in data){
 	    			var o = new Option(data[state], state);
